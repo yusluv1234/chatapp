60345b9832fab0e22455e8a46f7bdb934c102fcf
yusluv1234 <yusluv124@gmail.com>
2021-07-14 19:49:30 +0100

    commenting

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(FlashChat());
 
-// class FlashChat extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData.dark().copyWith(
-//         textTheme: TextTheme(
-//           bodyText2: TextStyle(color: Colors.black54),
-//         ),
-//       ),
-//       initialRoute: WelcomeScreen.id,
-//       routes: {
-//         WelcomeScreen.id: (context) => WelcomeScreen(),
-//         LoginScreen.id: (context) => LoginScreen(),
-//         RegistrationScreen.id: (context) => RegistrationScreen(),
-//         ChatScreen.id: (context) => ChatScreen()
-//       },
-//     );
-//   }
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
+    );
+  }
 }
